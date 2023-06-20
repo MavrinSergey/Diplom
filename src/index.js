b681768b89c57a09542781d3a61a0d70097aa75d
MavrinSergey <mavrin_sergei@list.ru>
2023-06-20 22:44:04 +0400

    get запрос

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ import { getScrumBoard, getItemScrumBoard } from './modules/Board';
 
 // const requestURL = 'https://jsonplaceholder.typicode.com/users';
 const requestURL = 'http://127.0.0.1:8000/api/v1/token/';
+const requestTask = 'http://127.0.0.1:8000/api/v1/task/'
 const openFormSign = document.getElementById("open-form-sign");
 const openScrumBoard = document.getElementById("open-ScrumBoard");
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg3MzIxMzUwLCJpYXQiOjE2ODcyMzQ5NTAsImp0aSI6ImMzYzMyMGJlMzA3NDRlNThhNDJlNDk5NTFlZWYxOGY0IiwidXNlcl9pZCI6MX0.rAK4N2tpuNeuQ4T0BEb37r7VCJaRfWni_e_Yiy7f91I'
+let access = '';
+let refresh = '';
+
 
 openFormSign.addEventListener("click", openModal);
 openScrumBoard.addEventListener("click", openBoard);
@@ -55,35 +58,31 @@ function openModal() {
         formData.forEach((value, key) => obj[key] = value);
         console.log('с формы')
         console.log(obj)
-        sendRequest('POST', requestURL, obj)
-            .then(data => console.log(data))
+        sendRequest('POST', requestURL, access, obj)
+            .then(data => saveToken(data))
             .catch(err => console.log(err))
         event.target.reset(); /* Сбрасывает форму */
         dropModalWindow(); /* закрывает окно*/
     }
+
 }
 
-// function saveToken(data) {
-//     access = data.access
-//     refresh = data.refresh
-// }
+function saveToken(data) {
+    access = data.access
+    refresh = data.refresh
+    console.log(access)
+    console.log(refresh)
+    sendRequest('GET', requestTask, access)
+        .then(data => Name(data))
+        .catch(err => console.log(err))
+}
 
 function Name(params) {
-    const test = `{"id_user": 1, 
-                    "name": "Sergey",
-                    "surname": "Mavrin",
-                    "id_task": [1, 2],
-                    "title": ["Заголовок задачи", "Заголовок задачи2"],
-                    "date_creation": ["14.06.2023", "14.06.2023"],
-                    "update_date": ["14.06.2023", "14.06.2023"],
-                    "status": ["agreement", "in-work"],
-                    "lead_time": ["31.06.2023", "30.06.2023"]}`;
-    const obj = JSON.parse(test);
     if (!document.getElementById("btnSign").querySelector("a")) {
-        createNameUser(obj.name + obj.surname);
+        createNameUser(params.name + params.surname);
     }
-    console.log(obj)
-    openBoard(obj);
+    console.log(params)
+    openBoard(params);
 }
 
 function openBoard(js=null) {
@@ -92,8 +91,9 @@ function openBoard(js=null) {
         createSection("board", getScrumBoard());
     }
 
-    // for (let i = 0; i < js.title.length; i++) {
-    //     createItemBoard(js.status[i], getItemScrumBoard(js.title[i], js.date_creation[i], js.update_date[i], js.lead_time[i]))
-    // }
+    for (let i = 0; i < js.length; i++) {
+        // console.log(js[i].title);
+        createItemBoard(js[i].status, getItemScrumBoard(js[i].title, js[i].date_creation, js[i].update_date, js[i].lead_time))
+    }
     
 }
--- src/modules/requests.js
+++ src/modules/requests.js
@@ -1,11 +1,11 @@
-export function sendRequest(method, url, body = null) {
+export function sendRequest(method, url, token, body = null) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
 
         xhr.open(method, url)
 
         xhr.responseType = 'json' // позволяет получать из респонса данные в json формате
-        xhr.setRequestHeader('Authorization', 'JWT ');
+        xhr.setRequestHeader('Authorization', 'JWT ' + token);
         xhr.setRequestHeader('Content-Type', 'application/json')
         xhr.onload = () => {
             if (xhr.status >= 400) {
@@ -22,3 +22,28 @@ export function sendRequest(method, url, body = null) {
         xhr.send(JSON.stringify(body))
     })
 }
+
+// export function sendRequestTask(method, url, token, body=null) {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest()
+
+//         xhr.open(method, url)
+
+//         xhr.responseType = 'json' // позволяет получать из респонса данные в json формате
+//         xhr.setRequestHeader('Authorization', 'JWT ' + token);
+//         xhr.setRequestHeader('Content-Type', 'application/json')
+//         xhr.onload = () => {
+//             if (xhr.status >= 400) {
+//                 reject(xhr.response); 
+//             } else {
+//                 resolve(xhr.response); // получаем данные в строке
+//             }
+//         }
+
+//         xhr.onerror = () => {
+//             reject(xhr.response);
+//         }
+
+//         xhr.send(JSON.stringify(body))
+//     })
+// }
