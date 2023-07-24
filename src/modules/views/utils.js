962bc8bc99581edf0a680ca7db66ad735037f491
MavrinSergey <mavrin_sergei@list.ru>
2023-07-24 20:04:50 +0400

    Addded views

---
+++ src/modules/views/utils.js
@@ -0,0 +1,56 @@
+import { bindDraggableCards } from "./dnd";
+
+export function createNameUser(content = null) {
+    const btnSign = document.getElementById("btnSign");
+    const openFormSign = document.getElementById("open-form-sign");
+    const name = document.createElement("a");
+    name.href = "#";
+    name.innerHTML = `${content}`;
+    btnSign.insertBefore(name, openFormSign);
+}
+
+export function createMainTitle(content = null) {
+    const title = document.createElement("div");
+    title.classList.add("main__title");
+    title.id = "main__title";
+    title.innerHTML = `${content}`;
+    main.appendChild(title);
+}
+
+export function createSection(title, content = null) {
+    const section = document.createElement("section");
+    section.classList.add(title);
+    section.id = title;
+    section.innerHTML = `${content}`;
+    main.appendChild(section);
+}
+
+export function createItemBoard(col, content = null, count = 0) {
+    const column = document.getElementById(`col${col}`);
+    if (!column) {
+        throw new Error(
+            `Column element with id col${col} not found in document`
+        );
+    }
+
+    const item = document.createElement("div");
+    item.classList.add("board-item");
+    item.dataset.item = count;
+
+    item.draggable = true;
+    item.innerHTML = `${content}`;
+    item.addEventListener("click", () => {
+        const check = item.querySelector("input");
+        check.checked = !check.checked;
+    });
+    column.appendChild(item);
+    bindDraggableCards();
+}
+
+export function createModal(title, content = null) {
+    const modal = document.createElement("div");
+    modal.classList.add(title);
+    modal.id = title;
+    modal.innerHTML = `${content}`;
+    document.body.appendChild(modal);
+}
