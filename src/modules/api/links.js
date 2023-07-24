66804ff25f34d42e15d1e01d3af177b80e57d1c9
MavrinSergey <mavrin_sergei@list.ru>
2023-07-24 19:43:02 +0400

    Added file links

---
+++ src/modules/api/links.js
@@ -0,0 +1,4 @@
+export const requestLogin = "http://127.0.0.1:8000/api/token/";
+export const requestRegistr = "http://127.0.0.1:8000/api/user/";
+export const requestTask = "http://127.0.0.1:8000/api/v1/task/";
+export const requestTaskDel = "http://127.0.0.1:8000/api/v1/taskdelete/";
